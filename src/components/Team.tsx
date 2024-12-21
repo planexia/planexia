f4e654780e87f717aeca1b4664d60d773c783e66
planexia <planexia.sa@gmail.com>
2024-12-21 14:47:50 -0300

    sdsd

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -18,7 +18,7 @@ const partners = [
 
 export default function Team() {
   return (
-    <div id="about" className="bg-blue-50 py-20">
+    <div id="about"  className="bg-blue-50 py-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-blue-900 sm:text-4xl">
